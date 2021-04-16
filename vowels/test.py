afee258949f0144b1f67e93c8f9cb55533fadf4d
amal-vinod <amalvinodb2000@gmail.com>
2021-04-16 18:43:11 +0530

    counting vowels

---
+++ vowels/test.py
@@ -0,0 +1,4 @@
+s="hfdsjgcdkjsbvhchduyeRTHSYIOSDGFGSBSKJfhsdavnjavir  ijnckjagjuhyfrtdsqdehjcvsvbhlhv oihvoghrvbvdbnca AscpafsdhgvshBgzhgfxjgf"
+
+print (sum(1 for let in s if let in "aeiouAEIOU"))
+
